42e27fc812ef09619955d0232160828f8d67a7ee
Simon Inns <simoninns@fastmail.com>
2025-03-30 18:03:27 +0200

    Fixed some of the .v headers with correct information

--- fpga/vp415-fpga/src/rgb111to666.v
+++ fpga/vp415-fpga/src/rgb111to666.v
@@ -3,25 +3,26 @@
     rgb111to666.v
     Convert RGB111 to RGB666
     
-    VP415-Emulator FPGA
-    Copyright (C) 2025 Simon Inns
-    
-    This file is part of VP415-Emulator.
-    
-    Domesday Duplicator is free software: you can redistribute it and/or
-    modify it under the terms of the GNU General Public License as
-    published by the Free Software Foundation, either version 3 of the
-    License, or (at your option) any later version.
-    
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
-    
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+	VP415-Emulator FPGA
+	Copyright (C) 2025 Simon Inns
+	
+	This file is part of VP415-Emulator.
+	
+	This is free software: you can redistribute it and/or
+	modify it under the terms of the GNU General Public License as
+	published by the Free Software Foundation, either version 3 of the
+	License, or (at your option) any later version.
+	
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+	
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-    Email: simon.inns@gmail.com
-    
+	Email: simon.inns@gmail.com
+	
 ************************************************************************/
 
 // Disable Verilog implicit definitions
